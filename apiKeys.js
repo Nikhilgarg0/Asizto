e746048474fec24cfa4a9c247e87dfafa13913cf
Nikhil <Nikhilgarg9755@cleargmail.com>
2025-10-06 03:57:27 +0530

    api

--- apiKeys.js
+++ apiKeys.js
@@ -1,3 +1,3 @@
 export const OPENROUTER_API_KEY = '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'
-//export const GEMINI_API_KEY = 'AIzaSyAPRfKIoRicEu04p1tVgEARIUZhWgaEEa0';
-export const GEMINI_API_KEY = 'AIzaSyAoD-9PuJ_8GWtDxwU7U-XhPIzFA7p_8Us';
+//export const GEMINI_API_KEY = 'AIzaSyA7DVD-DyHJzcURnNcbclMxdwc6jlmMrEQ';
+export const GEMINI_API_KEY = 'AIzaSyA7DVD-DyHJzcURnNcbclMxdwc6jlmMrEQ';
