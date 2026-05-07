0a02e81205e175f20d62591632baec2bb6af5b74
Nikhil <nikhilgarg9755@gmail.com>
2026-05-07 07:02:37 +0530

    deps fixing

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,10 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-          safe: true,
-          allowUndefined: false,
-        },
-      ],
-      'react-native-reanimated/plugin',  // ← moved to last
-    ],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+    }],
+    'react-native-reanimated/plugin',
+  ],
 };
